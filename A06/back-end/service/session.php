4cb5faccb1a931c0ccef4d8042fc6a7d8eca9b06
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-05-06 02:44:51 +0300

    start working on storing the username

--- A06/back-end/service/session.php
+++ A06/back-end/service/session.php
@@ -1,9 +1,11 @@
 <?php
 
 error_log("[log][session.php] started");
+error_log("\$_POST = " . json_encode($_POST));
+error_log("\$_REQUEST = " . json_encode($_REQUEST));
 if ($_POST['action'] == 'start') {
     session_start();
     $_SESSION['username'] = $_POST['username'];
     error_log("[log][session.php] username: ${_POST['username']}");
-    echo $_POST['username'];
+    echo json_encode($_POST['username']);
 }
